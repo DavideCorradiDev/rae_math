52ad4899f8c52c81548c9c7d15a65b85de07514f
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-26 21:57:49 +0200

    Fixed formatting

--- src/geometry2/to_homogeneous3.rs
+++ src/geometry2/to_homogeneous3.rs
@@ -2,39 +2,35 @@ use super::HomogeneousMatrix;
 use crate::{geometry3, matrix::RealField};
 
 use super::{
-  Affine, Isometry, OrthographicProjection, Projective, Rotation, Similarity,
-  Transform, Translation,
+    Affine, Isometry, OrthographicProjection, Projective, Rotation, Similarity, Transform,
+    Translation,
 };
 
-pub trait ToHomogeneous3<N: RealField>
-{
-  fn to_homogeneous3(&self) -> geometry3::HomogeneousMatrix<N>;
+pub trait ToHomogeneous3<N: RealField> {
+    fn to_homogeneous3(&self) -> geometry3::HomogeneousMatrix<N>;
 }
 
 fn homogeneous2_to_homogeneous3<N: RealField>(
-  m2: HomogeneousMatrix<N>,
-) -> geometry3::HomogeneousMatrix<N>
-{
-  let mut out = geometry3::HomogeneousMatrix::<N>::identity();
-  out[(0, 0)] = m2[(0, 0)];
-  out[(0, 1)] = m2[(0, 1)];
-  out[(0, 3)] = m2[(0, 2)];
-  out[(1, 0)] = m2[(1, 0)];
-  out[(1, 1)] = m2[(1, 1)];
-  out[(1, 3)] = m2[(1, 2)];
-  out
+    m2: HomogeneousMatrix<N>,
+) -> geometry3::HomogeneousMatrix<N> {
+    let mut out = geometry3::HomogeneousMatrix::<N>::identity();
+    out[(0, 0)] = m2[(0, 0)];
+    out[(0, 1)] = m2[(0, 1)];
+    out[(0, 3)] = m2[(0, 2)];
+    out[(1, 0)] = m2[(1, 0)];
+    out[(1, 1)] = m2[(1, 1)];
+    out[(1, 3)] = m2[(1, 2)];
+    out
 }
 
 macro_rules! implement_to_homogeneous3 {
-  ($StructType:ty) => {
-    impl<N: RealField> ToHomogeneous3<N> for $StructType
-    {
-      fn to_homogeneous3(&self) -> geometry3::HomogeneousMatrix<N>
-      {
-        homogeneous2_to_homogeneous3(self.to_homogeneous())
-      }
-    }
-  };
+    ($StructType:ty) => {
+        impl<N: RealField> ToHomogeneous3<N> for $StructType {
+            fn to_homogeneous3(&self) -> geometry3::HomogeneousMatrix<N> {
+                homogeneous2_to_homogeneous3(self.to_homogeneous())
+            }
+        }
+    };
 }
 
 implement_to_homogeneous3!(Affine<N>);
